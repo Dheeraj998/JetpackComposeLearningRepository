b593f7487ddc2c06697589a3f0659b862451fea0
dheeraj.artifitia <dheeraj.artifitia@gmail.com>
2024-12-09 15:37:41 +0530

    first commit

---
+++ app/src/main/java/com/artifitia/jetpackcomposelearning/TestScreen.kt
@@ -0,0 +1,9 @@
+package com.artifitia.jetpackcomposelearning
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TestScreen(modifier: Modifier = Modifier) {
+    
+}
